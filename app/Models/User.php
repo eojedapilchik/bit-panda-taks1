9e9b490c5f3ef86f3b71386f2c8026dcd0e57e59
Alcides Ojeda <eojedapilchik@gmail.com>
2022-02-15 20:20:21 +0100

    added User Controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
-        'password',
+        'active',
     ];
 
     /**
@@ -39,6 +38,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
     ];
 }
